06aad33b41986d069d260e41882f87e4549f7ef7
buffaloyoungcoding <78521392+buffaloyoungcoding@users.noreply.github.com>
2021-09-24 10:44:52 +0700

    Update J01022.java

--- J01022.java
+++ J01022.java
@@ -9,10 +9,11 @@ public class J01022 {
     static int Try(int n, long k){
         if(n == 1) return 0;
         if(n == 2) return 1;
-        if(k <= fibo[n-2]) return Try(n-2, k);
-        else return Try(n-1, k-fibo[n-2]);
+        if(k <= fibo[n-2]) return Try(n-2, k); // neu k <= do dai cua xau n-2 thi ki tu can tim nam o vi tri k trong xau n-2
+        else return Try(n-1, k-fibo[n-2]); // else nam o vi tri k - do dai cua xau n-2 cua xau n-1
     }
     static void setUp(){
+        // khoi tao do dai cua cac xau fibonacci
         fibo[0] = 0;
         fibo[1] = 1;
         for(int i = 2; i < 93; i++)
