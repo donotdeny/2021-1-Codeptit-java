d4165e3dff0585718b83ed1072838c2c8ee12fe5
buffaloyoungcoding <78521392+buffaloyoungcoding@users.noreply.github.com>
2021-09-29 21:16:26 +0700

    Create J01009.java

---
+++ J01009.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class temp {
+    static long gt(int n){
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        return n*gt(n-1);
+    }
+    static String toString(long n){
+        String res = "";
+        while(n > 0){
+            long tmp = n % 10;
+            res += (char)(tmp + '0');
+            n /= 10;
+        }
+        res = new StringBuffer(res).reverse().toString();
+        return res;
+    }
+    static boolean smaller(String s1, String s2){
+        if(s1.length() > s2.length()) return false;
+        else if(s1.length() < s2.length()) return  true;
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) < s2.charAt(i)) return true;
+            else if(s1.charAt(i) > s2.charAt(i)) return false;
+        }
+        return false;
+    }
+    static String sum(String s1, String s2){
+        int flag = 0;
+        String res = "";
+        if(smaller(s1, s2)){
+            String t = s1;
+            s1 = s2;
+            s2 = t;
+        }
+        s1 = new StringBuffer(s1).reverse().toString();
+        s2 = new StringBuffer(s2).reverse().toString();
+        for(int i = 0; i < s2.length(); i++){
+            int tmp = (int)(s1.charAt(i) - '0') + (int)(s2.charAt(i) - '0') + flag;
+            if(tmp >= 10){
+                tmp -= 10;
+                flag = 1;
+            }
+            else flag = 0;
+            res += (char)(tmp + '0');
+        }
+        for(int i = s2.length(); i < s1.length(); i++){
+            int tmp = (int)(s1.charAt(i) - '0') + flag;
+            if(tmp >= 10){
+                tmp -= 10;
+                flag = 1;
+            }
+            else flag = 0;
+            res += (char)(tmp + '0');
+        }
+        if(flag == 1){
+            res += "1";
+        }
+        res = new StringBuffer(res).reverse().toString();
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int n = ip.nextInt();
+        String res = "1";
+        for(int i = 2; i <= n; i++){
+            long tmp = gt(i);
+            res = sum(res, toString(tmp));
+        }
+        System.out.println(res);
+        ip.close();
+    }
+}
